11ac86c8213e2978a4d76d44c708805fff05e127
Daniel Garbeto Cardoso <garbetod@gmail.com>
2025-10-14 13:47:07 -0300

    AAAAAAAAAAAAAAAAAAAAAAA

---
+++ banif-front/src/routes.js
@@ -0,0 +1,40 @@
+import { createBrowserRouter, } from "react-router-dom";
+
+import Login from './pages/login/Login';
+/*
+import SignUp from './pages/signUp/SignUp';
+import Home from './pages/home/Home';
+import RegisterEvent from "./pages/registerEvent/RegisterEvent";
+import ResetPassword from "./pages/resetPassword/ResetPassword";
+*/
+
+const router = createBrowserRouter([
+    { 
+        path: "/", 
+        element: <Login /> 
+    },
+
+    /*
+    { 
+        path: "/signUp", 
+        element: <SignUp /> 
+    },
+    
+    {
+    path: "/home", 
+        element: <Home /> 
+    },
+    
+    {
+    path: "/registerEvent", 
+        element: <RegisterEvent /> 
+    },
+    
+    {
+    path: "/resetPassword", 
+        element: <ResetPassword /> 
+    },
+    */
+])
+
+export default router
